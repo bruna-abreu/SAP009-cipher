504eb5faa2eca0292dcad00876901ae945c5a1b2
bruna-abreu <bruna.abreu95@gmail.com>
2023-01-18 19:39:28 -0300

    primeira-interação

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+document.getElementById("btn1").addEventListener("click",encrypt);
+
+function encrypt(){
+  const text1 = document.getElementById("box1").value;
+  document.getElementById("box2").value = text1 
+}
