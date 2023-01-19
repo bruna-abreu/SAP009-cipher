0181c0d2958ede9e80bf37b73b5a70c21a8dc91f
bruna-abreu <bruna.abreu95@gmail.com>
2023-01-19 18:44:07 -0300

    nova-function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import cipher from './cipher.js';
 
+const text = document.getElementById("box1"); 
 document.getElementById("btn1").addEventListener("click",encrypt);
+document.getElementById("btn2").addEventListener("click",decrypt);
 
 function encrypt(){
-  const text1 = document.getElementById("box1").value;
-  document.getElementById("box2").value = text1 
+  const msg = text.value.toUpperCase(); 
+  document.getElementById("box2").value = msg; 
+}
+
+function decrypt() {
+  const msg = text.value.toUpperCase();
+  document.getElementById("box2").value = msg;
 }
